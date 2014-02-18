6badff3877277c5cce65ee5da6a10c904964fc18
GuiceU <peres.mans@gmail.com>
2014-02-18 16:10:49 +0100

    Update Dodger.java

--- Dodger.java
+++ Dodger.java
@@ -1,11 +1,13 @@
-package com.guiceu.lyricsfinder.SPEL;
+package wingman.dodger;
 
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Point;
+import android.util.Log;
 import android.view.Display;
+import android.view.MotionEvent;
 import android.view.WindowManager;
 
 public class Dodger {
@@ -15,20 +17,28 @@ public class Dodger {
     private int xCoord;
     private int yCoord;
 
+    private int mScreenWidth;
+    private int mScreenHeight;
+
+    private int mPosition;
+
     public Dodger(Context mContext, int mDrawable, int yCoord) {
         this.mView = BitmapFactory.decodeResource(mContext.getResources(), mDrawable);
         this.yCoord = yCoord;
         this.mContext = mContext;
+        initX();
     }
 
-    private void init() {
+    private void initX() {
         WindowManager wm = (WindowManager) mContext.getSystemService(Context.WINDOW_SERVICE);
         Display display = wm.getDefaultDisplay();
         Point size = new Point();
         display.getSize(size);
-        int mScreenWidth = size.x;
+        mScreenWidth = size.x;
+        mScreenHeight = size.y;
         int mViewWidth = mView.getWidth();
         xCoord = mScreenWidth / 2 - (mViewWidth / 2);
+        mPosition = 2;
     }
 
     public Bitmap getView() {
@@ -55,13 +65,31 @@ public class Dodger {
         this.xCoord = xCoord;
     }
 
+    public void move (boolean mRight) {
+
+        if (mRight) {
+            if(mPosition == 2) {
+
+            }
+        }
+
+
+    }
+
     public void resetCoords() {
-        init();
+        initX();
     }
 
     public void draw(Canvas canvas) {
         canvas.drawBitmap(mView, xCoord, yCoord, null);
     }
 
-
+    public void onTouch(MotionEvent event) {
+        switch(event.getAction()) {
+            case MotionEvent.ACTION_MOVE:
+                Log.d("sdgfg", "movemove");
+                setxCoord((int) event.getX());
+                break;
+        }
+    }
 }
